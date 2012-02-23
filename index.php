5e1b0ca8a9aee61736d94e8d5b7a24f6a6345f64
Zoran Lazic <ezelohar@gmail.com>
2012-02-23 20:37:40 +0100

    Metafix

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ require_once('functions.php');
     <head>
         <title>ShiftPlanning</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-
+        <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
         <link rel="stylesheet" type="text/css" href="css/style.mobile.css" />
         <script src="js/sp.user.js" type="text/javascript"></script>
         <script type="text/javascript">
