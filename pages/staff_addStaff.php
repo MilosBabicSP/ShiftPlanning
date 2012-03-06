190bb36b1e1e51d00239ad512c7a591f6d803c70
Emil <emil@shiftplanning.com>
2012-03-06 12:57:15 +0100

    staff htmls

--- pages/staff_addStaff.php
+++ pages/staff_addStaff.php
@@ -1,3 +1,80 @@
 <div class="main addStaff">
-    asdf
+    <div class="title">
+        <h3 class="icoEmpAdd">Add New Employee</h3>
+    </div>
+    <ul class="detailsGrid">
+        <li>
+            <div>
+                <label>Name:</label>
+                <span class="input">
+                    <input type="text" name="" value="">
+                </span>
+            </div>
+        </li>
+        <li>
+            <div>
+                <label>Nick Name:</label>
+                <span class="input">
+                    <input type="text" name="" value="">
+                </span>
+            </div>
+        </li>
+        <li>
+            <div>
+                <label>Email:</label>
+                <span class="input">
+                    <input type="text" name="" value="">
+                </span>
+            </div>
+        </li>
+        <li>
+            <div>
+                <label>Employee ID:</label>
+                <span class="input">
+                    <input type="text" name="" value="">
+                </span>
+            </div>
+        </li>
+        <li>
+            <div>
+                <label>Username:</label>
+                <span class="input">
+                    <input type="text" name="" value="">
+                </span>
+            </div>
+        </li>
+        <li>
+            <div>
+                <label>Hourly Wage:</label>
+                <span class="input">
+                    <input type="text" name="" value="">
+                </span>
+            </div>
+        </li>
+    </ul>
+    <ul class="detailsGrid">
+        <li>
+            <div class="title">
+                <h3 class="icoNot">Notes</h3>
+            </div>
+        </li>
+        <li>
+            <span class="input">
+                <textarea>Write Notes...</textarea>
+            </span>
+        </li>
+    </ul>
+    <div class="title">
+        <h3 class="icoAct">Activation</h3>
+    </div>
+    <div class="title">
+        <span class="fl"><span class="checkbox">Send Activation</span></span>
+    </div>
+    <div class="additional">
+        <p>If this box is checked and account is setup with an e-mail address a welcome e-mail will be sent to this employee with their login instructions.</p>
+        <p>If left un-checked, they will be silently added to the system and can be sent their activation email or given their login details later.</p>
+    </div>
+    <div class="title">  
+        <span class="fr"><a href="#">Create Employee</a></span>      
+    </div>
 </div>
--- pages/staff_fastAssignment.php
+++ pages/staff_fastAssignment.php
@@ -1,3 +1,72 @@
 <div class="main fastAssignment">
-    asdf
+    <div class="title">
+        <h3 class="icoSta">Staff</h3>
+    </div>
+    <ul class="detailsGrid">
+        <li>
+            <label>Choose Employee</label>
+            <span class="input">
+                <select name="">
+                    <option>asdsad</option>
+                    <option>asdsad</option>
+                </select>
+            </span>
+        </li>
+    </ul>
+    <div class="title">
+        <h3 class="icoEmpPos">Positions</h3>
+    </div>
+    <ul class="detailsGrid">
+        <ul>    
+            <li class="even">
+                <div>
+                    <span class="checkbox">Development</span>
+                </div>
+            </li>
+            <li class="odd">
+                <div>
+                    <span class="checkbox">Development</span>
+                </div>
+            </li>
+            <li class="even">
+                <div>
+                    <span class="checkbox">Development</span>
+                </div>
+            </li>
+            <li class="odd">
+                <div>
+                    <span class="checkbox">Development</span>
+                </div>
+            </li>
+        </ul>
+    </ul>
+    <div class="title">
+        <h3 class="icoEmpSki">Skills</h3>
+    </div>
+    <ul class="detailsGrid">
+        <li>
+            <ul>    
+                <li class="even">
+                    <div>
+                        <span class="checkbox">Development</span>
+                    </div>
+                </li>
+                <li class="odd">
+                    <div>
+                        <span class="checkbox">Development</span>
+                    </div>
+                </li>
+                <li class="even">
+                    <div>
+                        <span class="checkbox">Development</span>
+                    </div>
+                </li>
+                <li class="odd">
+                    <div>
+                        <span class="checkbox">Development</span>
+                    </div>
+                </li>
+            </ul>
+        </li>
+    </ul>
 </div>
