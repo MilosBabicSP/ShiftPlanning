ec297666221c5b47c5c2eade8381cc07631f8d2f
sveta Milenkovic <svetamilenkovic@gmail.com>
2012-04-26 14:20:31 +0200

    lang select mobile

--- config.php
+++ config.php
@@ -4,5 +4,6 @@ define('DEBUGGER', true);
 //define('API_KEY','49e4207b8459ef88e19de9b78b882ef15ec14d84');
 //define('API_URL','http://www.dejan1-s.dev.shiftplanning.com/api/');
 
-define('API_KEY','18eb10a76f1e97fd20b0248fd5475bdbc8996c5e');
+//define('API_KEY','18eb10a76f1e97fd20b0248fd5475bdbc8996c5e');
+define('API_KEY','49e4207b8459ef88e19de9b78b882ef15ec14d84');
 define('API_URL','http://www.translations-s.dev.shiftplanning.com/api/');
