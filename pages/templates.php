5a09d8c3d16d7e2a8859e8143e33082b4a0a9cf4
sveta Milenkovic <svetamilenkovic@gmail.com>
2012-04-30 14:37:43 +0200

    list timesheets

--- pages/templates.php
+++ pages/templates.php
@@ -846,16 +846,7 @@
     <script id="te_tc_dts_li" type="text/x-jquery-tmpl">
         <li>
             <span class="names">${status}</span>
-            <span class="time">
-                <span class="tStart">${st.time}</span>
-                <img width="16" height="16" src="images/tc_sm_clock.png" />
-                {{if length.total_hours == ""}}
-                    <span class="tPending" user="${user}">Clock Out</span>
-                {{else}}
-                    <span class="tEnd">${out.time}</span>
-                {{/if}}
-            </span>
-            <span class="last">${length.hours}h, ${length.mins}min</span>
+
         </li>
     </script>
     <script id="te_tc_mts_li" type="text/x-jquery-tmpl">
