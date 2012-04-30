955991d84a6db6ffce37233b8397d78331a0756c
sveta Milenkovic <svetamilenkovic@gmail.com>
2012-04-30 13:28:19 +0200

    list timesheets

--- pages/timeClock_displayTimeSheets.php
+++ pages/timeClock_displayTimeSheets.php
@@ -2,19 +2,19 @@
     <div class="title">
         <h3>My Time Sheets</h3>
     </div>
-</div>
-<div>
-    <span class="input">
-        <select id="tc_dts_au">
-            <option value="0" >All</option>
-            <option value="1" >Approved</option>
-            <option value="2" selected="selected">Unapproved</option>
-        </select>
-    </span>
-</div>
-<div>
-    <span class="input">
-        <select id="tc_dts_tr">
-        </select>
-    </span>
+    <div>
+        <span class="input">
+            <select id="tc_dts_au">
+                <option value="0" >All</option>
+                <option value="1" >Approved</option>
+                <option value="2" selected="selected">Unapproved</option>
+            </select>
+        </span>
+    </div>
+    <div>
+        <span class="input">
+            <select id="tc_dts_tr">
+            </select>
+        </span>
+    </div>    
 </div>
