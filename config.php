f36e6564672e3ef88cc4338f3385ed0664e019a0
sveta Milenkovic <svetamilenkovic@gmail.com>
2012-05-02 15:44:29 +0200

    whosonnow mobile

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 define('DEBUGGER', true);
-//define('API_KEY','49e4207b8459ef88e19de9b78b882ef15ec14d84');
-//define('API_URL','http://www.dejan1-s.dev.shiftplanning.com/api/');
+define('API_KEY','49e4207b8459ef88e19de9b78b882ef15ec14d84');
+define('API_URL','http://www.dejan1-s.dev.shiftplanning.com/api/');
 
-define('API_KEY','18eb10a76f1e97fd20b0248fd5475bdbc8996c5e');
-define('API_URL','http://www.shiftplanning.com/api/');
+//define('API_KEY','18eb10a76f1e97fd20b0248fd5475bdbc8996c5e');
+//define('API_URL','http://www.shiftplanning.com/api/');
