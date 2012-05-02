31ce36b64b886f4fed6c1c452b6337f5282b78c4
sveta Milenkovic <svetamilenkovic@gmail.com>
2012-05-02 15:32:41 +0200

    whosonnow mobile

--- js/sp.dashboard.mobile.js
+++ js/sp.dashboard.mobile.js
@@ -436,7 +436,7 @@ ShiftPlanningDashboard.prototype.settingsSubEvents = function(employee){
 
     ShiftPlanningDashboard.prototype.whosonnowSubEvents = function(){
         console.log('Whosonnow SubEvents')
-        spModel.schedule.get('shifts', {mode:'employee'}, function(response){
+        spModel.schedule.get('shifts', {mode:'onnow'}, function(response){
             console.log(response.data);
         })
     }
