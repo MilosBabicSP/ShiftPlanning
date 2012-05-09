52941b33c70a38b9f1fe93b845654e472cbb8e49
Zoran Lazic <ezelohar@gmail.com>
2012-05-09 12:05:55 +0200

    latest

--- config.php
+++ config.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 define('DEBUGGER', true);
+define('_root_', $_SERVER['DOCUMENT_ROOT'].'/shiftplanning-mobile/');
+define('_jsV_', 1);
+define('WWW_PATH', 'http://'.$_SERVER['SERVER_NAME'].'/shiftplanning-mobile/');
 //define('API_KEY','49e4207b8459ef88e19de9b78b882ef15ec14d84');
 //define('API_URL','http://www.dejan1-s.dev.shiftplanning.com/api/');
 
---
+++ jspacker.php
@@ -0,0 +1,74 @@
+<?php
+
+class JSPacker {
+
+    private $output = '';
+    private $scripts = array();
+    private $build = true;
+    public $reset = true;
+    public $_store = false;
+
+    function __construct($output) {
+	$this->output = $output;
+	$this->build = file_exists(_root_ . 'js/' . _jsV_ . $this->output) ? false : true;
+
+	# WRITE TO SCREEN
+    }
+
+    function _dump() {
+
+	//we check do we wanna create single file
+	if ($this->_store) {
+	    $this->_toHtml();
+	} else {
+	    $this->_build();
+	}
+    }
+
+    function _add($file, $compress = false) {
+	if (strpos($script['path'], 'ttp:') || strpos($script['path'], 'ttps:')) {
+	    array_push($this->scripts, array('path' => $file, 'pack' => $compress));
+	} else {
+	    array_push($this->scripts, array('path' => WWW_PATH . $file, 'pack' => $compress));
+	}
+	
+    }
+
+    function _toHtml() {
+	foreach ($this->scripts as $script) {
+	    # REMOTE SCRIPT
+	    echo '<script type="text/javascript" src="' . $script['path'] . '"></script>';
+	}
+    }
+
+    function _build() {
+	
+	# FORCE BUILD
+	if ($this->reset) {
+	    $this->build = true;
+	}
+
+	if (is_array($this->scripts) && $this->build) {
+
+	    # GRAB FILES
+	    foreach ($this->scripts as $script) {
+		$tmp = file_get_contents($script['path']);
+		$output[] = $tmp;
+		unset($tmp);
+	    }
+	    
+	    # WRITE TO OUTPUT
+	    chmod(_root_ . 'js', 0777);
+	    if (file_exists(_root_ . 'js/' . _jsV_ . $this->output)){
+		chmod(_root_ . 'js/' . _jsV_ . $this->output, 0777);
+	    }
+//	    
+	    file_put_contents(_root_ . 'js/' . _jsV_ . $this->output, implode("\n ", $output));
+	}
+	
+	//delete old file
+	unlink(WWW_PATH . 'js/' . _jsV_ . $this->output);
+	echo '<script type="text/javascript" src="' . WWW_PATH . 'js/' . _jsV_ . $this->output . '"></script>';
+    }
+}
+
