f24170e3207c1f5c5a11a4de22c6ee240d774350
Zoran Lazic <ezelohar@gmail.com>
2012-05-09 12:23:10 +0200

    latest

--- config.php
+++ config.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 if (strpos($_SERVER['SERVER_NAME'], '192.168.1')){
     define('_ext_' , 'shiftplanning-mobile/');
 } else {
