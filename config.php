7d4c9ee2ab7f9e727bbce3ce7a455073aa15060a
Zoran Lazic <ezelohar@gmail.com>
2012-05-09 12:21:29 +0200

    latest

--- config.php
+++ config.php
@@ -10,7 +10,7 @@ if (strpos($_SERVER['SERVER_NAME'], '192.168.1')){
 
 define('DEBUGGER', true);
 define('_root_', $_SERVER['DOCUMENT_ROOT'].'/'._ext_);
-define('_jsV_', 1);
+define('_jsV_', 2);
 define('WWW_PATH', 'http://'.$_SERVER['SERVER_NAME'].'/'._ext_);
 //define('API_KEY','49e4207b8459ef88e19de9b78b882ef15ec14d84');
 //define('API_URL','http://www.dejan1-s.dev.shiftplanning.com/api/');
