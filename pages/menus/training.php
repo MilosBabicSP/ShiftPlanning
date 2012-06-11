6a02c33b62518b7f8e7039f288da9dd2086309d8
Emil <emil@shiftplanning.com>
2012-06-11 14:01:54 +0200

    training bar

--- pages/menus/training.php
+++ pages/menus/training.php
@@ -1,13 +1,13 @@
 <div class="training">
-	<a href="#" class="toggleMenu">
+    <a href="#" class="toggleMenu">
         <img width="24" height="24" src="<?php echo _fCdnPath_; ?>images/ShowHideMenu.png" />
     </a>
-	<div class="subWrapp">
+    <div class="subWrapp">
+        <div class="trainingBar">
             Training progress : 93% Completed
-            <div class="trainingBar">
-                <div class="progress" style="width:70%">
-                    
-                </div>
+            <div class="barholder">
+                <div class="progress" style="width:70%"></div>
             </div>
-	</div>
+        </div>
+    </div>
 </div>
