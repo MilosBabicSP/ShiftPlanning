68a911655f1a8929932d32fd4d80bd6fd034b95a
Emil <emil@shiftplanning.com>
2012-06-11 13:50:06 +0200

    training bar

--- pages/menus/training.php
+++ pages/menus/training.php
@@ -3,13 +3,10 @@
         <img width="24" height="24" src="<?php echo _fCdnPath_; ?>images/ShowHideMenu.png" />
     </a>
 	<div class="subWrapp">
-		<ul class="subNav fl" page="training">
-			<li class="first active">
-                <a href="#" subpage="overview">
-                    <img width="16" height="16" src="<?php echo _fCdnPath_; ?>images/rep_1.png" />
-                </a>
-            </li>
- 
-		</ul>
+            <div class="trainingBar">
+                <div class="progress" style="width:70%">
+                    
+                </div>
+            </div>
 	</div>
 </div>
