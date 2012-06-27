2507e04b5a2b2360ca2cdcc29c3a17eae0bc4458
sveta Milenkovic <svetamilenkovic@gmail.com>
2012-06-27 13:54:12 +0200

    training new

--- pages/training_statistic.php
+++ pages/training_statistic.php
@@ -1,4 +1,4 @@
-<div class="main statistic" id="tr_stat">
-	<ul class="training_statistic staff big">
+<div class="main statistic" id="tr_stat" style="padding: 0px;">
+	<ul class="training_statistic staff big" >
 	</ul>	
 </div>
