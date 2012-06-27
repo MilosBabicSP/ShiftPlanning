ad04ce716af8f31ddaa22fd54ab26ae9b16a1419
sveta Milenkovic <svetamilenkovic@gmail.com>
2012-06-27 13:30:07 +0200

    training new

--- pages/training_overview.php
+++ pages/training_overview.php
@@ -1,6 +1,6 @@
 <div class="main sections" id="tr_ov">
 	<div style="display: block;" class="title">
-		<h3 class="fl">Select section to get started.</h3>   
+		<h3 class="fl">Select section to start with.</h3>   
 	</div>
 	<ul class="timeSheet training_sections">
 	</ul>	
