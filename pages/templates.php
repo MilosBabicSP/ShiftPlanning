3ae3d5e9a022de034d136a2ca7184cdf083acccd
sveta Milenkovic <svetamilenkovic@gmail.com>
2012-06-27 17:45:47 +0200

    touch

--- pages/templates.php
+++ pages/templates.php
@@ -801,7 +801,7 @@
 	<script id="te_tr_sections" type="text/x-jquery-tmpl">
 		<li class="idle">
 					<div rel="${id}" >
-			<a href="#" class="next fr" ><img width="43" height="30" src="<?php echo _fCdnPath_; ?>images/NextMenu.png"></a>
+			<div href="#" class="next fr" ><img width="43" height="30" src="<?php echo _fCdnPath_; ?>images/NextMenu.png"></div>
 			<span class="oneLine">
                             <b>${title}</b>{{if notfinished_count > 0}}<a style="padding-left:10px;"><img style="width:16px;height:16px;padding-bottom:3px;" src="<?php echo _fCdnPath_;?>images/req_1.png"></a> {{/if}}
 			</span>
@@ -832,7 +832,7 @@
 	<script id="te_tr_singleSection" type="text/x-jquery-tmpl">
 		<li class="idle">
 					<div rel="${id}" >
-			<a href="#" class="next fr" ><img width="43" height="30" src="<?php echo _fCdnPath_; ?>images/NextMenu.png"></a>
+			<div href="#" class="next fr" ><img width="43" height="30" src="<?php echo _fCdnPath_; ?>images/NextMenu.png"></div>
 			<span class="oneLine">
                             <b>${title}</b> {{if finished_flag == 99 || finished_flag == 0}}<a style="padding-left:10px;"><img style="width:16px;height:16px;padding-bottom:3px;" src="<?php echo _fCdnPath_;?>images/req_1.png"></a> 
 												{{else}}{{if finished_flag == 1 }} <a style="padding-left:10px;"><img style="width:16px;height:16px;padding-bottom:3px;" src="<?php echo _fCdnPath_;?>images/tc_approve.png"></a> {{/if}}
