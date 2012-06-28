e86bdea8b363a7bdcbe598f2da99cd8f9c11772e
sveta Milenkovic <svetamilenkovic@gmail.com>
2012-06-28 16:30:47 +0200

    m

--- js/sp.common.mobile.js
+++ js/sp.common.mobile.js
@@ -12,10 +12,12 @@ jQuery.event.special.touch = {
     setup: function(data,namespaces){
         var elem = this, $elem = jQuery(elem);
         if (window.Touch) {
+			aleret($elem);
             $elem.bind('touchstart', jQuery.event.special.touch.onTouchStart);
             $elem.bind('touchmove', jQuery.event.special.touch.onTouchMove);
             $elem.bind('touchend', jQuery.event.special.touch.onTouchEnd);
         } else {
+			aleret($elem);
             $elem.bind('click', jQuery.event.special.touch.click);
         }
     },
