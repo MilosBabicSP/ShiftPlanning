1681f3e5979f299fcf49e5c6c229213594c47d1f
sveta Milenkovic <svetamilenkovic@gmail.com>
2012-06-28 12:15:46 +0200

    training

--- pages/menus/training.php
+++ pages/menus/training.php
@@ -3,7 +3,7 @@
         <img width="24" height="24" src="<?php echo _fCdnPath_; ?>images/ShowHideMenu.png" />
     </a>
     <div class="subWrapp">
-		<ul class="subNav training" page="training">
+		<ul class="subNav training fl" page="training">
 				<li class="first active">
 					<a subpage="sections" href="#">
 						<img width="16" height="16" src="<?php echo _fCdnPath_;?>images/book-open-bookmark.png">
@@ -15,8 +15,8 @@
 					</a>
 				</li>
 				</ul>
-        <div class="trainingBar hidden">
-            Training progress : <span id="user_progress">0</span>% Completed
+        <div class="trainingBar">
+            <span id="user_progress">0</span>% Completed
             <div class="barholder">
                 <div class="progress" style="width:0%"></div>
             </div>
