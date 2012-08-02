a5225d16a711fed9690e9cfcd71cabcd5ff26266
Zoran Lazic <ezelohar@gmail.com>
2012-08-02 12:13:26 +0200

    wrong time fix

--- functions.php
+++ functions.php
@@ -199,7 +199,7 @@ class Functions{
     function getCurrentTime(){
 	$this->timezone();
         var_dump(($this->settings['24hr'] == 1));
-	return '<time>' . date($this->settings['date']) . '</time><span>' .  date($this->settings['date'] . " - " . ($this->settings['24hr'] == 1) ? "H:i" : "g:ia") . '</span>';
+	return '<time>' . date($this->settings['date']) . '</time><span>' .  date(($this->settings['24hr'] == 1) ? "H:i" : "g:ia") . '</span>';
     }
     
     
