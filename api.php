e2b92f9b09250f7661a797ee88708bd4d3c65e66
Zoran Lazic <ezelohar@gmail.com>
2012-08-16 17:09:17 +0200

    file fixing

--- api.php
+++ api.php
@@ -86,9 +86,14 @@ if($_POST['module'] && $_POST['module'] != 'admin.file'){
 			return false;
 		}
 		$file = str_replace($data['data']['extension'],  strtoupper($data['data']['extension']), $data['data']['filename']);  
+                header("Pragma: public");
+                header("Expires: 0");
+                header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+                header("Cache-Control: public");
+                header("Content-Description: File Transfer");
                 header('Content-Type: application/octet-stream');
-                header('Content-Disposition: attachment; filename="radi.TXT"');
-                //header("Content-Length: " . $data['data']['file_size']);
+                header('Content-Disposition: attachment; filename="'.$file.'"');
+                header("Content-Length: " . $data['data']['file_size']);
 
 		echo $return;
 	}else{
