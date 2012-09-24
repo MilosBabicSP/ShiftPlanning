d6064c6a2ddb810686caba86ada343522d7444a6
Zoran Lazic <ezelohar@gmail.com>
2012-09-24 17:56:37 +0200

    fix

--- js/sp.common.mobile.gap.js
+++ js/sp.common.mobile.gap.js
@@ -1,20 +1,18 @@
-var sp = {};
+var sp = new ShiftPlanning();
 $(document).ready(function() {
     $.ajax({
         url: 'load.php',
         success : function(res) {
             $('#prepLoadFiles').after(res);
-            console.log(sp, 'test', ShiftPlanning);
-            //sp = new ShiftPlanning();
-//            ShiftPlanning.prototype.staff = new ShiftPlanningStaff();
-//            ShiftPlanning.prototype.schedule = new ShiftPlanningSchedule();
-//            ShiftPlanning.prototype.dashboard = new ShiftPlanningDashboard();
-//            ShiftPlanning.prototype.timeClock = new ShiftPlanningTimeClock();
-//            ShiftPlanning.prototype.reports = new ShiftPlanningReports();
-//            ShiftPlanning.prototype.requests = new ShiftPlanningRequests();
-//            ShiftPlanning.prototype.location = new ShiftPlanningLocation();
-//            ShiftPlanning.prototype.permissions = new ShiftPlanningPermissions();
-//            ShiftPlanning.prototype.training = new ShiftPlanningTraining();
+            ShiftPlanning.prototype.staff = new ShiftPlanningStaff();
+            ShiftPlanning.prototype.schedule = new ShiftPlanningSchedule();
+            ShiftPlanning.prototype.dashboard = new ShiftPlanningDashboard();
+            ShiftPlanning.prototype.timeClock = new ShiftPlanningTimeClock();
+            ShiftPlanning.prototype.reports = new ShiftPlanningReports();
+            ShiftPlanning.prototype.requests = new ShiftPlanningRequests();
+            ShiftPlanning.prototype.location = new ShiftPlanningLocation();
+            ShiftPlanning.prototype.permissions = new ShiftPlanningPermissions();
+            ShiftPlanning.prototype.training = new ShiftPlanningTraining();
         }
     })
 });
