7fb0eb8de11edbe0ad9798f9889c84a2e9db6bb4
Zoran Lazic <ezelohar@gmail.com>
2012-09-24 15:25:38 +0200

    fix

--- js/sp.common.mobile.gap.js
+++ js/sp.common.mobile.gap.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
         url: 'load.php',
         success : function(res){
             $('#prepLoadFiles').after(res);
-            sp = new ShiftPlanning();
+            //sp = new ShiftPlanning();
 //            ShiftPlanning.prototype.staff = new ShiftPlanningStaff();
 //            ShiftPlanning.prototype.schedule = new ShiftPlanningSchedule();
 //            ShiftPlanning.prototype.dashboard = new ShiftPlanningDashboard();
