3fdf7a6bc5e095fa8ac3af2610275b9a294ed4e2
Zoran Lazic <ezelohar@gmail.com>
2012-10-01 16:41:34 +0200

    add api url

--- functions.php
+++ functions.php
@@ -32,6 +32,9 @@ class Functions{
     
     function timezone(){
 	$this->settings = json_decode(_iapi(array('module' => 'admin.settings', 'method' => 'GET'), 'json', true), true);
+        $employee = json_decode(_iapi(array('module' => 'staff.employee', 'method' => 'GET', 'id' => $_GET['id']), 'json', true), true);
+        
+        print_r($employee);
 	$timezones['-12:00,0'] = '(-12:00) International Date Line West';
 	$tzPhp_val['-12:00,0'] = 'Etc/GMT+12';
 
