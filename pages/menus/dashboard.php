be466c775768a611b29e59c45b58dc24adff9da2
Emil <emil@shiftplanning.com>
2012-11-07 13:33:48 +0100

    css update

--- pages/menus/dashboard.php
+++ pages/menus/dashboard.php
@@ -6,32 +6,32 @@
         <ul class="subNav" page="dashboard">
             <li class="first active">
                 <a href="#" subpage="wall">
-                    <img width="16" height="16" src="<?php echo _fCdnPath_;?>images/Dashboard_Home16x16.png" />
+                    <img width="16" height="17" src="<?php echo _fCdnPath_;?>images/Dashboard_Home16x16.png" />
                 </a>
             </li>
             <li>
                 <a href="#" subpage="upcomingShifts">
-                    <img width="16" height="16" src="<?php echo _fCdnPath_;?>images/Dashboard_UpcomingShifts16x16.png" />
+                    <img width="16" height="17" src="<?php echo _fCdnPath_;?>images/Dashboard_UpcomingShifts16x16.png" />
                 </a>
             </li>
             <li id="da_up_fi_hide">
                 <a href="#" subpage="files">
-                    <img width="16" height="16" src="<?php echo _fCdnPath_;?>images/uploaded-files.png" />
+                    <img width="16" height="17" src="<?php echo _fCdnPath_;?>images/uploaded-files.png" />
                 </a>
             </li>
             <li class="hidden">
                 <a href="#" subpage="whosonnow">
-                    <img width="16" height="16" src="<?php echo _fCdnPath_;?>images/Dashboard_whosonnow.png" />
+                    <img width="16" height="17" src="<?php echo _fCdnPath_;?>images/Dashboard_whosonnow.png" />
                 </a>
             </li>            
             <li>
                 <a href="#" subpage="inbox">
-                    <img width="16" height="16" src="<?php echo _fCdnPath_;?>images/Dashboard_Inbox16x16.png" />
+                    <img width="16" height="17" src="<?php echo _fCdnPath_;?>images/Dashboard_Inbox16x16.png" />
                 </a>
             </li>
             <li class="last">
                 <a href="#" subpage="settings">
-                    <img width="16" height="16" src="<?php echo _fCdnPath_;?>images/Dashboard_Settings16x16.png" />
+                    <img width="16" height="17" src="<?php echo _fCdnPath_;?>images/Dashboard_Settings16x16.png" />
                 </a>
             </li>
         </ul>
