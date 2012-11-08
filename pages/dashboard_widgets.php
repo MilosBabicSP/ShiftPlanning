7aa87cd055c4c2640c7e4ae509e94391889e746a
Emil <emil@shiftplanning.com>
2012-11-08 14:24:08 +0100

    dashboard

--- pages/dashboard_widgets.php
+++ pages/dashboard_widgets.php
@@ -1,5 +1,63 @@
 <div class="main dashboard" id="da_widgets">
-	
-	wwidgetssss
-	
+    <ul class="widgets">
+        <li class="timeClock">
+            <a href="#">
+                <span class="icon">
+
+                </span>
+                <span class="details">
+                    <h3>Time Clock</h3>
+                    <p>You are not clocked in</p>
+                </span>
+            </a>
+        </li>
+        <li class="timeClock clocked">
+            <a href="#">
+                <span class="icon">
+
+                </span>
+                <span class="details">
+                    <h3>Time Clock</h3>
+                    <p>Clocked in for <b>4h 36min</b></p>
+                </span>
+            </a>
+        </li>
+        <li class="shifts">
+            <a href="#">
+                <span class="icon">
+                    <b>12</b>
+                </span>
+                <span class="details">
+                    <p>Shifts available<br/>for <b>trade</b></p>
+                </span>
+            </a>
+        </li>
+        <li class="shifts">
+            <a href="#">
+                <span class="icon">
+                    <b>8</b>
+                </span>
+                <span class="details">
+                    <p>Shifts available<br/>for <b>pick-up</b></p>
+                </span>
+            </a>
+        </li>
+        <li class="schedule">
+            <a href="#">
+                <span class="icon">
+                    <p>OCT</p>
+                    <h4>31</h4>
+                </span>
+                <span class="details">
+                    <h3>Scheduler</h3>
+                    <p>You have <b>2 shifts</b></p>
+                </span>
+            </a>
+        </li>
+    </ul>
+    <ul class="shifts">
+        <li>
+            
+        </li>
+    </ul>
 </div>
