2158b590c7f968946a6dfa14fb7ab902d00dedc5
Emil <emil@shiftplanning.com>
2012-11-14 13:59:43 +0100

    mobile menus update

--- pages/menus/requests.php
+++ pages/menus/requests.php
@@ -6,27 +6,27 @@
         <ul class="subNav fl" page="requests">
             <li class="first active">
                 <a href="#" subpage="overview">
-                    <img width="16" height="16" src="<?php echo _fCdnPath_;?>images/req_1.png" />
+                    <img width="16" height="17" src="<?php echo _fCdnPath_;?>images/req_1.png" />
                 </a>
             </li>
             <li>
                 <a href="#" subpage="vacation">
-                    <img width="16" height="16" src="<?php echo _fCdnPath_;?>images/req_2.png" />
+                    <img width="16" height="17" src="<?php echo _fCdnPath_;?>images/req_2.png" />
                 </a>
             </li>
             <li class="hidden">
                 <a href="#" subpage="openShifts">
-                    <img width="16" height="16" src="<?php echo _fCdnPath_;?>images/req_3.png" />
+                    <img width="16" height="17" src="<?php echo _fCdnPath_;?>images/req_3.png" />
                 </a>
             </li>
             <li>
                 <a href="#" subpage="shiftTrades">
-                    <img width="16" height="16" src="<?php echo _fCdnPath_;?>images/req_4.png" />
+                    <img width="16" height="17" src="<?php echo _fCdnPath_;?>images/req_4.png" />
                 </a>
             </li>
             <li class="last ">
                 <a href="#" subpage="shiftApprovals">
-                    <img width="16" height="16" src="<?php echo _fCdnPath_;?>images/req_5.png" />
+                    <img width="16" height="17" src="<?php echo _fCdnPath_;?>images/req_5.png" />
                 </a>
             </li>
         </ul>
--- pages/menus/training.php
+++ pages/menus/training.php
@@ -6,12 +6,12 @@
 		<ul class="subNav training fr" page="training">
 				<li class="first active">
 					<a subpage="sections" href="#">
-						<img width="16" height="16" src="<?php echo _fCdnPath_;?>images/book-open-bookmark.png">
+						<img width="16" height="17" src="<?php echo _fCdnPath_;?>images/book-open-bookmark.png">
 					</a>
 				</li>
 				<li class="hidden">
 					<a subpage="statistic" href="#">
-						<img width="16" height="16" src="<?php echo _fCdnPath_;?>images/stats.png">
+						<img width="16" height="17" src="<?php echo _fCdnPath_;?>images/stats.png">
 					</a>
 				</li>
 				</ul>
