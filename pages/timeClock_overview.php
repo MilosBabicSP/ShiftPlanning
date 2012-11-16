6abad3aaee85f2e955c57a2ae35d22b61e106f6e
Zoran Lazic <ezelohar@gmail.com>
2012-11-16 13:10:49 +0100

    add

--- pages/timeClock_overview.php
+++ pages/timeClock_overview.php
@@ -7,21 +7,34 @@
             <span>13:55</span>
         </div>
     </div>
-    <div class="newMsg hidden wide" id="tc_ov_cf">
-        <label><?=_s('Schedule')?></label>
-        <span class="input">
-            <select name="" id="tc_ov_ss">
-            </select>
-        </span>
-        <label><?=_s('Notes')?></label>
-        <span class="input">
-            <textarea id="tc_ov_no"></textarea>
-        </span>
-
-        <div class="title">
-            <span class="fr"><a href="#" id="tc_ov_ca"><?=_s('Cancel')?></a></span>
-            <span class="fl"><a href="#" id="tc_ov_sa"><?=_s('Save')?></a></span>
+    <div class="hidden" id="tc_ov_cf">
+        <div class="newMsg hidden wide" id="tc_ov_cf">
+            <label><?=_s('Schedule')?></label>
+            <span class="input">
+                <select name="" id="tc_ov_ss">
+                </select>
+            </span>
+            <label><?=_s('Notes')?></label>
+            <span class="input">
+                <textarea id="tc_ov_no"></textarea>
+            </span>
+            <div class="title">
+                <span class="fr"><a href="#" id="tc_ov_ca"><?=_s('Cancel')?></a></span>
+                <span class="fl"><a href="#" id="tc_ov_sa"><?=_s('Save')?></a></span>
+            </div>
         </div>
+        <ul class="step">
+            <li>
+                <div>
+                    <a href="#" id="tc_ov_ca" class="grey"><?=_s('Cancel')?></a>
+                </div>
+            </li>
+            <li>
+                <div>
+                    <a href="#" id="tc_ov_sa" class="green"><?=_s('Save')?></a>
+                </div>
+            </li>
+        </ul>
     </div>
     <div class="additional" id="tc_ov_ad">
 	<p><?=_s('You cannot Clock in/out from this mobile device. Management has restricted clock time to specific locations.');?></p>
