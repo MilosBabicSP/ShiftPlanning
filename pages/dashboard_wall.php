79e5e99e6f2b6cf345549e55ce73460a4db6e4e5
Zoran Lazic <ezelohar@gmail.com>
2012-11-16 12:24:26 +0100

    add

--- pages/dashboard_wall.php
+++ pages/dashboard_wall.php
@@ -3,19 +3,29 @@
         <span class="fr"><a href="#" id="da_wa_nm_b"><?=_s('New Message')?></a></span>
         <h3><?=_s('Message Wall')?></h3>
     </div>
-    <div class="newMsg hidden wide" id="da_wa_nm_f">
-        <label><?=_s('Title')?></label>
-        <span class="input">
-            <input type="text" name="" value="" id="da_wa_nm_ti" />
-        </span>
-        <label><?=_s('Message')?></label>
-        <span class="input">
-            <textarea id="da_wa_nm_me"></textarea>
-        </span>
-        <div class="title">
-            <span class="fr"><a href="#" id="da_wa_nm_sa"><span><?=_s('Save Message')?></span></a></span>
-            <span class="fl"><a href="#" id="da_wa_nm_ca"><?=_s('Cancel')?></a></span>
+    <div id="da_wa_nm_f" class="hidden">
+        <div class="newMsg wide">
+            <label><?=_s('Title')?></label>
+            <span class="input">
+                <input type="text" name="" value="" id="da_wa_nm_ti" />
+            </span>
+            <label><?=_s('Message')?></label>
+            <span class="input">
+                <textarea id="da_wa_nm_me"></textarea>
+            </span>
         </div>
+        <ul class="step tradepick">
+                <li>
+                        <div>
+                                <a href="#" id="da_wa_nm_sa"><span><?=_s('Save Message')?></span></a>
+                        </div>
+                </li>
+                <li>
+                        <div>
+                                <a href="#" id="da_wa_nm_ca"><?=_s('Cancel')?></a>
+                        </div>
+                </li>
+        </ul>
     </div>
     <ul class="msgWall" id="da_wa_li">
 
