601a7b2b71c0a668541e518e3c7f36f35ca3bf8d
Zoran Lazic <ezelohar@gmail.com>
2012-11-30 03:25:06 +0100

    css fixes

--- pages/timeClock_displayTimeSheets.php
+++ pages/timeClock_displayTimeSheets.php
@@ -1,34 +1,37 @@
 <div class="main displayTimeSheets" id="tc_dts">
     <div class="title">
-        <h3><?=_s('My Time Sheets');?></h3>
+        <h3><?= _s('My Time Sheets'); ?></h3>
     </div>
-    <ul class="detailsGrid">
-        <li>
-            <ul>
-                <li class="even">
-            <div>
-                <span class="input">
-                    <select id="tc_dts_au">
-                        <option value="0" selected="selected"><?=_s('All');?></option>
-                        <option value="1" ><?=_s('Approved');?></option>
-                        <option value="2" ><?=_s('Unapproved');?></option>
-                    </select>
-                </span>
-            </div>
-        </li>
-        <li class="odd">
-            <div>
-                <span class="input">
-                    <select id="tc_dts_tr">
-                    </select>
-                </span>
-            </div>   
-        </li>
-    </ul>
-    </ul>
+    <div>
+        <ul class="detailsGrid">
+            <li>
+                <ul>
+                    <li class="even">
+                        <div>
+                            <span class="input">
+                                <select id="tc_dts_au">
+                                    <option value="0" selected="selected"><?= _s('All'); ?></option>
+                                    <option value="1" ><?= _s('Approved'); ?></option>
+                                    <option value="2" ><?= _s('Unapproved'); ?></option>
+                                </select>
+                            </span>
+                        </div>
+                    </li>
+                    <li class="odd">
+                        <div>
+                            <span class="input">
+                                <select id="tc_dts_tr">
+                                </select>
+                            </span>
+                        </div>   
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>            
     <ul class="timeSheet" id="tc_dts_ul">
 
     </ul>
-            <div id="tc_dts_ul_msg">
-        </div> 
+    <div id="tc_dts_ul_msg">
+    </div> 
 </div>
