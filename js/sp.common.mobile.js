bdc9d82a9e53d9b8921ab47d72d36ae08c802a3c
Zoran Lazic <ezelohar@gmail.com>
2012-11-30 04:03:09 +0100

    css fixes

--- js/sp.common.mobile.js
+++ js/sp.common.mobile.js
@@ -104,7 +104,7 @@ ShiftPlanning.prototype.loadSubPage = function(obj, page, subpage) {
     }
     
     sp.fixCheckboxes();
-    $(document).scrollTop(0);
+    //$(document).scrollTop(0);
 }
 
 ShiftPlanning.prototype.initialize = function(){
