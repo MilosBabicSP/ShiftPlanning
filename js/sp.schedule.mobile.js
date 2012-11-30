d0fa2145803ee46dd06449cbee6bf55f100863da
Zoran Lazic <ezelohar@gmail.com>
2012-11-30 03:49:45 +0100

    css fixes

--- js/sp.schedule.mobile.js
+++ js/sp.schedule.mobile.js
@@ -741,7 +741,7 @@ ShiftPlanningSchedule.prototype.addShiftSubEvents = function(){
         
         $.each(emp.staff, function(i, item){
             console.log(item);
-            if (item == null ){
+            if ( item == null || item.length == 0 ){
                 $('#sc_add_user div[type=' + i + ']').hide();
             } else {
                 $('#sc_add_user div[type=' + i + '] ul.detailsGrid li ul').html($.tmpl($('#te_sc_users'), self.prepareStaff(item)));
