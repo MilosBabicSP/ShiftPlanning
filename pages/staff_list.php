e773f6f4c5681501afa8af10e8dda615dd634d4f
Zoran Lazic <ezelohar@gmail.com>
2012-12-11 17:51:20 +0100

    fix 5

--- pages/staff_list.php
+++ pages/staff_list.php
@@ -1,4 +1,12 @@
 <div class="main list" id="st_li">
+    <div class="title">
+        <div class="middle">
+            <?= _s('Grid View');?>
+        </div>
+        <div class="middle">
+            <?= _s('List View');?>
+        </div>
+    </div>
     <ul class="staff big" id="st_li_ga"> <!--big-->
     </ul>
     <div class="noResults hidden"><?=_s('There are no results for searched query.')?></div>
