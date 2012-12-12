7c72782d672900d29236287ba9dec85b1010a008
Zoran Lazic <ezelohar@gmail.com>
2012-12-12 18:38:15 +0100

    fix 4

--- js/sp.common.mobile.js
+++ js/sp.common.mobile.js
@@ -252,7 +252,6 @@ ShiftPlanning.prototype.initialize = function(){
         });
         
         $('.blackMask').bind('drag', function(e) {
-            console.log(e);
             if (parseInt(e.distanceX) > 50 && e.direction == 'left'){
                 start = true;
             }
