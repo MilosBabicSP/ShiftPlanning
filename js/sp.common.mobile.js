fd84850cf96b2056129aad3471d94a86eee6d45e
Zoran Lazic <ezelohar@gmail.com>
2012-12-12 17:15:10 +0100

    fix 4

--- js/sp.common.mobile.js
+++ js/sp.common.mobile.js
@@ -214,6 +214,7 @@ ShiftPlanning.prototype.initialize = function(){
         }
         
         $('.wrapper').bind('swipe', function(e) {
+            console.log('test');
             if (e.direction == 'right') {
                 $('#menu').removeClass('hidden');
                 $('#wrapper').addClass('extended');
