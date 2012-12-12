3c808bd3dcf788560a0d06202ca76473a0175a22
Zoran Lazic <ezelohar@gmail.com>
2012-12-12 11:48:22 +0100

    fix 5

--- pages/staff_list.php
+++ pages/staff_list.php
@@ -1,12 +1,20 @@
 <div class="main list" id="st_li">
     <div class="staffMenu" id="st_tp_menu">
-        <div class="middle left active" id="st_sn_ga">
+        <div class="middle left active">
             <?= _s('Grid View');?>
         </div>
-        <div class="middle right" id="st_sn_li">
+        <div class="middle right">
             <?= _s('List View');?>
         </div>
     </div>
+    <table width="100%" id="st_tp_menu">
+        <tbody>
+            <tr>
+                <td id="st_sn_ga"><?= _s('Grid View');?></td>
+                <td id="st_sn_li"><?= _s('List View');?></td>
+            </tr>
+        </tbody>
+    </table>
     <ul class="staff big" id="st_li_ga"> <!--big-->
     </ul>
     <div class="noResults hidden"><?=_s('There are no results for searched query.')?></div>
