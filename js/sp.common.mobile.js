e180f65002c88b286cc884b73664324c10bee3bb
Zoran Lazic <ezelohar@gmail.com>
2012-12-12 18:42:46 +0100

    fix 4

--- js/sp.common.mobile.js
+++ js/sp.common.mobile.js
@@ -267,7 +267,6 @@ ShiftPlanning.prototype.initialize = function(){
                 e.distanceX = 190;
             }
             if (start) {
-                console.log(e.distanceX);
                 $('#wrapper').css('margin-left', parseInt(e.distanceX));
                 $('#menu').css('margin-left',(-190 + parseInt(e.distanceX)) );   
             }
