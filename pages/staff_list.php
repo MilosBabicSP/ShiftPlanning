64ac0a10b67454b3bbc4aa1720c89e270c273f32
Zoran Lazic <ezelohar@gmail.com>
2012-12-12 12:42:41 +0100

    fix 5

--- pages/staff_list.php
+++ pages/staff_list.php
@@ -2,7 +2,7 @@
     <table width="100%" id="st_tp_menu">
         <tbody>
             <tr>
-                <td id="st_sn_ga"><?= _s('Grid View');?></td>
+                <td id="st_sn_ga" class="active"><?= _s('Grid View');?></td>
                 <td style="border-right:1px solid #DBDBDB;width:0px;"></td>
                 <td id="st_sn_li"><?= _s('List View');?></td>
             </tr>
