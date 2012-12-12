fa5adec059d2a45ab6a8cbb71c01015f99c68201
Zoran Lazic <ezelohar@gmail.com>
2012-12-12 12:02:17 +0100

    fix 5

--- pages/staff_list.php
+++ pages/staff_list.php
@@ -11,7 +11,7 @@
         <tbody>
             <tr>
                 <td id="st_sn_ga"><?= _s('Grid View');?></td>
-                <td  style="border-right:1px solid #DBDBDB;width:0px;"></td>
+                <td style="border-right:1px solid #DBDBDB;width:0px;"></td>
                 <td id="st_sn_li"><?= _s('List View');?></td>
             </tr>
         </tbody>
