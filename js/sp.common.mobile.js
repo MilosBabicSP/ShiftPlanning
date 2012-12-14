2edc7933970425756adeeba9251b99828d9973c1
Zoran Lazic <ezelohar@gmail.com>
2012-12-14 14:52:41 +0100

    fix 4

--- js/sp.common.mobile.js
+++ js/sp.common.mobile.js
@@ -63,9 +63,13 @@ ShiftPlanning.prototype.toggleMenu = function(){
     if ($('#wrapper').hasClass('extended')){
         $('#wrapper').css('margin-left', 190);
         $('#menu').css('margin-left', 0);
+        $('.blackMask').css('display','block');
+        $('.blackMask').css('opacity','0.5');
     } else {
         $('#wrapper').css('margin-left', 0);
         $('#menu').css('margin-left', -190);
+        $('.blackMask').css('display','none');
+        $('.blackMask').css('opacity','0');
     }
 }
 
