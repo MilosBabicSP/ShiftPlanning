df072eb06ccf973b75482e4c28db4274173483d3
Zoran Lazic <ezelohar@gmail.com>
2012-12-24 21:25:39 +0100

    fix a bug

--- config.php
+++ config.php
@@ -41,7 +41,7 @@ if (IS_TEST_SERVER) {
 }
 
 
-define('_jsV_', 17);
+define('_jsV_', 18);
 define('WWW_PATH', 'http://' . $_SERVER['SERVER_NAME'] . '/' . _ext_);
 define('LANG_PATH', 'http://' . $_SERVER['SERVER_NAME'] . '/');
 
