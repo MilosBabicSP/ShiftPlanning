06cdf796fdddafe6dc09b934c02cacfe33048eb1
Zoran Lazic <ezelohar@gmail.com>
2012-12-24 15:55:15 +0100

    fix a bug

--- js/sp.settings.mobile.js
+++ js/sp.settings.mobile.js
@@ -150,15 +150,15 @@ ShiftPlanningSettings.prototype.overviewSubEvents = function(employee){
     if (employee.id == sp.staff.admin.info.id){
 	$('#settings .search').show();
 	if (!sp.permissions.hasPermission('edit_profile')){
-	    $('#settings .subNav a[subpage=edit]').hide();
+	    $('.subNavigation .settings .subNav a[subpage=edit]').hide();
 	} else {
-	    $('#settings .subNav a[subpage=edit]').show();
+	    $('.subNavigation .settings .subNav a[subpage=edit]').show();
 	}
     } else {
 	if (sp.staff.admin.info.group > 4){
 	    $('#settings .search').hide();
 	} else {
-	    $('#settings .subNav a[subpage=edit]').show();
+	    $('.subNavigation .settings .subNav a[subpage=edit]').show();
 	    $('#settings .search').show();
 	}
     }
