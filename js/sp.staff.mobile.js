a9b7fdaa5eec9dd78d2752385a2014635663f4ef
Zoran Lazic <ezelohar@gmail.com>
2012-12-25 12:42:57 +0100

    fix

--- js/sp.staff.mobile.js
+++ js/sp.staff.mobile.js
@@ -290,9 +290,9 @@ ShiftPlanningStaff.prototype.login = function(){
                                         setCookie('shiftplanning_mobile_usercompany', user.company, cookieExpire);
                                         setCookie('shiftplanning_mobile_userphone', user.phone, cookieExpire);
                                     }
-//                                    setTimeout(function() {
-//                                        
-//                                    }, 2000);
+                                    setTimeout(function() {
+                                        $('.applicationContainer').fadeIn(500);
+                                    }, 2000);
                             });
                     });
             });
