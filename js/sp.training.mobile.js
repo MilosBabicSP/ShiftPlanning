e178c853310d1ba3fb375f1b45ee686e25bd7e89
Zoran Lazic <ezelohar@gmail.com>
2012-12-25 10:34:30 +0100

    fix a bug

--- js/sp.training.mobile.js
+++ js/sp.training.mobile.js
@@ -105,7 +105,7 @@ ShiftPlanningTraining.prototype.sectionsSubEvents = function (){
 		$('.training  a[subpage=statistic]').parent().removeClass('hidden');
 		$('.training .singleModule .subMenu').removeClass('hidden');		
 	}else{
-		$('.trainingBar').css('margin','0 60px 0 0 ');
+		//$('.trainingBar').css('margin','0 60px 0 0 ');
 	}
 	$('.training_sections').html(spView.ulLoader());
 	var s=[];
