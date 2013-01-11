0c36e555e6a53bbff30c08ccbd86d716dec0492f
Zoran Lazic <ezelohar@gmail.com>
2013-01-11 13:33:35 +0100

    add

--- js/sp.common.mobile.js
+++ js/sp.common.mobile.js
@@ -102,7 +102,7 @@ ShiftPlanning.prototype.loadSubPage = function(obj, page, subpage) {
     
     $('.subNavigation div.' + page + ' .subnNav[page=' + page + '] li a[page=' + subpage + ']').parent().addClass('active');
     sp.hashChange = false;
-    //sp.hash(page);
+    sp.hash(page);
     
     if (typeof this[page] != 'undefined' && 'loadSubPageEvents' in this[page]){
         this[page].loadSubPageEvents(subpage);
@@ -112,7 +112,7 @@ ShiftPlanning.prototype.loadSubPage = function(obj, page, subpage) {
     if (page == 'schedule' && subpage == 'addShift') {
         console.log('test');
     } else {
-        //$(window).scrollTop(0);
+        $(window).scrollTop(0);
     }
 }
 
