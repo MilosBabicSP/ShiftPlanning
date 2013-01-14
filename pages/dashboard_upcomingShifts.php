976ac0a0abdbdb79de698a4d9814a1ac04007d3a
Zoran Lazic <ezelohar@gmail.com>
2013-01-14 11:25:00 +0100

    add

--- pages/dashboard_upcomingShifts.php
+++ pages/dashboard_upcomingShifts.php
@@ -2,7 +2,7 @@
     <div class="title">
         <h3><?= _s('Upcoming shifts'); ?></h3>
     </div>
-    <ul class="timeSheet" id="da_up_li">
+    <ul class="shifts moved" id="da_up_li">
 
     </ul>
 </div>
