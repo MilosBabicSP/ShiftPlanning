229a30d084219ad353113e46bbd73343a44ca726
Vladimir Vujosevic <vladimir.vujosevic@shiftplanning.com>
2013-03-28 19:03:14 +0100

    wip assets packer

--- assetspacker.php
+++ assetspacker.php
@@ -1,35 +1,158 @@
 <?php
 
-if(!defined(')root_')) define('_root_', dirname(__FILE__) . '/');
+if(!defined('_root_')) define('_root_', dirname(__FILE__) . '/');
 include(_root_ . 'config.php');
 
+//var_dump($packer);
 
 /**
  * Class AssetsPacker
- * 
- * - cron-like assets packer
+ *
  */
 
 class AssetsPacker {
 	
 	private $_output = "";
-	private $_scripts  = array();
 	private $_build = false;
+	
 	private $_build_path = "static/";
+	private $build_file_path;
+
+	public $assets;
+	public $type;
 	
-	public $reset = false;
-	public $store = false;
-	public $type = "js";
 	
-	
-	function __construct($output = "") {
+	function __construct($assets = array(), $type = array()) {
+		$this->assets = $assets;
+		$this->type = $type;
+		
+		//define build file path
+		$this->build_file_path = _root_ . $this->_build_path . _jsV_;
+		
+		//check what to build
+		if( in_array('css', $this->type) ) {
+			$this->buildCssAssets( $this->assets );
+		}
+		
+		if( in_array('js', $this->type) ) {
+			$this->buildJsAssets( $this->assets );
+		}
 		
 	}
 	
 	
-	public function build() {
+	/**
+	* Build CSS assets method
+	* 
+	*/
+	
+	public function buildCssAssets($assets=array()) {
 		
-	}	
+		
+		if(!file_exists($this->build_file_path . '.build')){
+			
+			//file_put_contents($this->build_file_path, ''); //create file
+			
+			if(!empty($assets)) {
+				
+				foreach($assets['css'] as $packedName => $list){
+					
+					$packed = array();
+					
+					foreach($list as $script) {
+						
+						if(strpos($script, 'ttp:') || strpos($script, 'ttps:')) {
+						
+							$tmp = file_get_contents($script);
+							
+						} else {
+							
+							ob_start();
+							include(_root_ . $script);
+							$tmp = ob_get_contents();
+							ob_end_clean();
+							
+						}
+						
+						$tmp .= "\n\n";
+						$packed[] = $tmp;
+					}
+					
+					$this->_output = str_replace(array("\r\n", "\n", "\r"), '', implode(' ', $packed));
+					//cache output name
+					$output_name = $this->build_file_path . $packedName;
+					file_put_contents($output_name . '.css', $this->_output);
+					file_put_contents($output_name . '.tmp.css', $this->_output);
+					
+					printf("Css file(s) written in {$output_name}.css \n");
+					
+				}
+				
+			} else {
+				
+				throw new Exception("Your assets array is empty. Nothing to build.");
+			
+			}
+			
+		}
+		
+	}
+	
+	/**
+	 *Build JS assets method
+	 *
+	 */
+	
+	public function buildJsAssets( $assets=array() ) {
+		
+		if(!file_exists($this->build_file_path . '.build')){
+			
+			if(!empty($assets)) {
+				
+				foreach ($assets['js'] as $packedName => $list) {
+					
+					foreach ($list as $script) {
+						
+						if(strpos($script, 'ttp:') || strpos($script, 'ttps:')){
+							
+							$tmp = file_get_contents($script);
+							
+						} else {
+							
+							ob_start();
+							include(_root_ . $script);
+							$tmp = ob_get_contents();
+							ob_end_clean();
+					
+						}
+						
+						$tmp .= "\n\n";
+						var_dump($script);
+						$packed[$packedName][] = $tmp;
+						
+					}
+					
+					$output_name = $this->build_file_path . $packedName;
+					file_put_contents($output_name . '.tmp.js', implode(' ', $packed[$packedName]));
+					printf("JS file(s) written in {$output_name}.tmp.js \n");
+					
+				}
+				
+				//TODO - Closure compiler 
+				
+			} else {
+				
+				throw new Exception("Your assets array is empty. Nothing to build.");
+			
+			}
+			
+		}
+		
+	}
 	
 	
 }
+
+$assets_to_pack = array('css', 'js');
+$ap = new AssetsPacker($packer, $assets_to_pack);
+//$ap->build($packer);
---
+++ config.assets.php
@@ -0,0 +1,91 @@
+<?php
+
+
+// const _version_
+
+define('_version_', 0);
+
+$packer = array();
+
+$packer['css'] = array(
+	"mobile" => array(
+		'css/add2home.css',
+		'css/style.mobile.css',
+		'css/style_12.mobile.css',
+		'css/mobiscroll.css'
+	)
+);
+
+$packer['js'] = array(
+	'external' => array(
+		'js/jquery/jquery-1.6.4.min.js',
+		'js/jquery/jquery-ui.js',
+		'js/jquery/jquery.ba-hashchange.min.js',
+		'js/jquery/jquery.contains.js',
+		'js/jquery/jquery.mobile-1.0.js',
+		'js/jquery/jquery.timeago.js',
+		'js/jquery/jquery.tmpl.js',
+		'js/jquery/jquery.toggleText.js',
+		'js/plugins/add2home.js',
+		'js/plugins/cookie.js',
+		'js/plugins/date.js',
+		'js/plugins/hammer.js',
+		'js/plugins/hash.js',
+		'js/plugins/jquery.hammer.js',
+		'js/plugins/jquery.specialevent.hammer.js',
+		'js/plugins/mobiscroll.js',
+		'js/plugins/objSort.js',
+		'js/plugins/outerClick.js',
+		'js/plugins/shorten.js',
+		'js/plugins/sp.cache.js',
+	),
+	
+	'mobile' => array(
+		'js/models/sp.admin.model.js',
+		'js/models/sp.location.model.js',
+		'js/models/sp.messaging.model.js',
+		'js/models/sp.payroll.model.js',
+		'js/models/sp.requests.model.js',
+		'js/models/sp.schedule.model.js',
+		'js/models/sp.staff.model.js',
+		'js/models/sp.timeclock.model.js',
+		'js/models/sp.training.model.js',
+		'js/schedule/date.extras.js',
+		'js/schedule/date.js',
+		'js/sp.admin.js',
+		'js/sp.common.js',
+		'js/sp.common.mobile.js',
+		'js/sp.common.mobile.gap.js',
+		'js/sp.dashboard.js',
+		'js/sp.dashboard.mobile.js',
+		'js/sp.location.js',
+		'js/sp.logout.js',
+		'js/sp.messaging.js',
+		'js/sp.modal.js',
+		'js/sp.model.js',
+		'js/sp.permissions.js',
+		'js/sp.permissions.mobile.js',
+		'js/sp.ranges.js',
+		'js/sp.reports.js',
+		'js/sp.reports.mobile.js',
+		'js/sp.requests.js',
+		'js/sp.requests.mobile.js',
+		'js/sp.schedule.js',
+		'js/sp.schedule.mobile.js',
+		'js/sp.settings.js',
+		'js/sp.settings.mobile.js',
+		'js/sp.shift.js',
+		'js/sp.staff.js',
+		'js/sp.staff.mobile.js',
+		'js/sp.timeclock.js',
+		'js/sp.timeclock.mobile.js',
+		'js/sp.training.js',
+		'js/sp.training.mobile.js',
+		'js/sp.user.js',
+		'js/sp.view.js'
+	)
+	
+	
+);
+//var_dump($packer);
+$GLOBALS['packer_sets'] = $packer;
