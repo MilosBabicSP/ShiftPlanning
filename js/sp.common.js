89c35d57d7f1a312fc4021f92af36ea0e3352557
sveta Milenkovic <svetamilenkovic@gmail.com>
2013-08-26 10:43:20 +0200

    clear console logs

--- js/sp.common.js
+++ js/sp.common.js
@@ -117,10 +117,8 @@ ShiftPlanning.prototype = {
     },
     loadPage: function(page){
         //Load the page from the module, handle this a little better
-		console.log('paggeee', page);
         if(user.loggedIn || page == 'privacy'){
             if(typeof this[page] != 'undefined'){
-				console.log('wuuut', $('#pages #' + page + ' > div'), $('#pages #' + page));
                 $('#pages #' + page + ' > div').hide();
                 $('#pages #' + page).show();
                 $('.subNavigation > div').hide();
--- js/sp.privacy.js
+++ js/sp.privacy.js
@@ -2,7 +2,6 @@ function ShiftPlanningPrivacy (){
 }
 
 ShiftPlanningPrivacy.prototype.loadPage = function(){
-    console.log('loooad page');
 	$('#pages>div').hide()
 	$('#pages #privacy').show();
 	$('#pages #privacy > div').show();
