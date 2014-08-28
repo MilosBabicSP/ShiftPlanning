ef3e243df153d95a81d90b0bff5d4d9872d2492b
Stevica Trajanovic <steewsc@gmail.com>
2014-08-28 09:30:20 +0200

    -

--- gapped/gapConfig.js
+++ gapped/gapConfig.js
@@ -6,9 +6,14 @@ var tcService = function(){};
 var tcServiceTimer = 30000;
 var myIPAddress = "";
 var appPath = "";
+
+/** This is an iOS API Key */
+var apiKey = "08b5181033e32284bb4f62b40ba00b721ed6282b";
 var isAndroid = false;
 if( navigator.userAgent.indexOf("Android") >= 0 ){
 	isAndroid = true;
+	/** This is an Android API Key */
+	apiKey = "1b1933e7e0a9fdeca1be7801ada2e04ad1719454";
 }
 
 // TODO: REPLACE API endpoint to LIVE
