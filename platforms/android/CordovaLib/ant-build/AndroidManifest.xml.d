25ffb14b3d1e6ffc4c0a39285b83d9d286c4e9b1
Milos Babic <milos.babic@shiftplanning.com>
2014-12-19 14:41:31 +0100

    removed ant builds

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/milos/Development/workspace/android/shiftplanning-mobile/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/home/milos/Development/workspace/android/shiftplanning-mobile/platforms/android/CordovaLib/AndroidManifest.xml \
